24f034bfc6b7a6d25f897fecfabef93bee740e99
ErikTjon <129341065+ErikTjon@users.noreply.github.com>
2023-03-29 19:19:28 -0500

    added west death

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 print("Welcome to the game")
 print("You're sitting at a cafe \n To the west is a busy street.")
+response = input()
+if (response == "go west"):
+    print("You failed to look both ways, and got hit by a trunk... and died.")
+else:
+    print("come again?")
